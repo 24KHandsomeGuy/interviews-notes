7f3d46f77fbe79003893645881c47f5f6fcf888a
fukuixiang <woaifkx1>
2022-06-21 00:06:23 +0800

    Reactor

--- demo-sample/src/main/java/com/bibabo/io/java/reactor/multimulti/MultiMultiReactor.java
+++ demo-sample/src/main/java/com/bibabo/io/java/reactor/multimulti/MultiMultiReactor.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
  * @Author: Damon Fu
  * @Date: 2022/6/20 22:07
  * @Description: 多线程多Reactor模型
- * MainReactor接收连接，交由Acceptor来处理，Acceptor处理后，将读写Handler注册到多线程个SubReactor上
+ * Acceptor接收连接器监听端口。MainReactor.select连接，就绪连接交由Acceptor来处理，Acceptor处理后，将读写Handler注册到多线程个SubReactor上
  * SubReactor如果发现有读写事件准备就绪，处理Handler
  */
 public class MultiMultiReactor {
